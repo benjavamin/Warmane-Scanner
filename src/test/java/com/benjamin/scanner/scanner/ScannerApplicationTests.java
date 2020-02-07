bff9eed6fa1d64bd1be21b95666737c84c0e7199
Benjamin <bajic.benjamin98@gmail.com>
2020-02-07 21:04:42 +0100

    Initial commit

---
+++ src/test/java/com/benjamin/scanner/scanner/ScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.scanner.scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
