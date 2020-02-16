a7df6d585ac730aa76739e964564ef0dc339b46c
Benjamin <bajic.benjamin98@gmail.com>
2020-02-16 02:32:27 +0100

    added UI elements

--- mobileapp/app/src/main/java/com/raywanderlich/arenaactivity/MainActivity.java
+++ mobileapp/app/src/main/java/com/raywanderlich/arenaactivity/MainActivity.java
@@ -4,12 +4,21 @@ package com.raywanderlich.arenaactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner bracketSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bracketSpinner = findViewById(R.id.bracketSpinner);
+        String[] bracketArr = new String[] {"2v2", "3v3"};
+        ArrayAdapter<String> brackets = new ArrayAdapter<>(this, bracketSpinner, bracketArr);
+        bracketSpinner.setAdapter(brackets);
     }
+
 }
